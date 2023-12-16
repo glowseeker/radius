31f56370920dfc0d2f7cf78ad2f0c58b36f88343
glowseeker <49524747+glowseeker@users.noreply.github.com>
2023-12-16 02:42:30 +0100

    decided on pyqtgraph

--- main.py
+++ main.py
@@ -1,109 +1,167 @@
-from matplotlib.patches import Circle
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, TextBox
+import math
+from PyQt5 import QtWidgets
 import numpy as np
+import pyqtgraph as pg
+from pyqtgraph.parametertree import Parameter, ParameterTree
 
-# Create figure and subplots
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+class MainWindow(QtWidgets.QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
 
-# Left pane - coordinate grid
-ax1.grid(True)
-ax1.axhline(0, color='black', linewidth=1)
-ax1.axvline(0, color='black', linewidth=1)
-ax1.set_xlim(-10, 10)
-ax1.set_ylim(-10, 10)
-ax1.set_aspect('equal')
+        # Create layout and add to central widget
+        self.layout = QtWidgets.QHBoxLayout()
+        self.widget = QtWidgets.QWidget()
+        self.setCentralWidget(self.widget)
+        self.widget.setLayout(self.layout)
 
-# Set ticks and labels
-ax1.set_xticks(range(-10, 11, 1))
-ax1.set_yticks(range(-10, 11, 1))
-ax1.xaxis.set_tick_params(labelsize=8)
-ax1.yaxis.set_tick_params(labelsize=8)
+        # Create parameters and tree
+        params = [
+            {'name': 'Inner Radius', 'type': 'float', 'value': 1, 'limits': (1, 10), 'step': 0.1},
+            {'name': 'Outer Radius', 'type': 'float', 'value': 10, 'limits': (1, 10), 'step': 0.1},
+            {'name': 'Starting Point', 'type': 'str', 'value': '(0, 0)', 'readonly': True},
+            {'name': 'Tangent Point', 'type': 'str', 'value': '(0, 0)', 'readonly': True},
+            {'name': 'Intersection Point', 'type': 'str', 'value': '(0, 0)', 'readonly': True},
+            {'name': 'Steps', 'type': 'int', 'value': 1, 'limits': (1, 1000), 'step': 1},
+        ]
+        self.params = Parameter.create(name='params', type='group', children=params)
+        self.parameter_tree = ParameterTree()
+        self.parameter_tree.setParameters(self.params, showTop=False)
 
-# Right pane - sliders and textboxes
-ax2.axis('off')  # Hide the axis for sliders
+        # Add parameter tree to layout
+        self.parameter_tree.setMinimumWidth(200)
+        self.layout.addWidget(self.parameter_tree)
 
-# Draw two circles
-inner_circle = Circle((0, 0), 1, fill=False, color='blue', linewidth=2)
-outer_circle = Circle((0, 0), 2, fill=False, color='red', linewidth=2)
-ax1.add_patch(inner_circle)
-ax1.add_patch(outer_circle)
+        # Create plot
+        self.plot_graph = pg.PlotWidget()
+        self.plot_graph.setBackground('w')
+        self.plot_graph.setAspectLocked(True)
+        self.plot_graph.showGrid(x=True, y=True)
+        self.plot_graph.getViewBox().setRange(xRange=(-10, 10), yRange=(-10, 10))
+        self.plot_graph.getAxis('bottom').setTickSpacing(1, 1)
+        self.plot_graph.getAxis('left').setTickSpacing(1, 1)
+        self.plot_graph.getViewBox().setMouseEnabled(x=False, y=False)
+        pen = pg.mkPen(width=2)
+        self.plot_graph.plot([-11, 11], [0, 0], pen=pen)  # X-axis
+        self.plot_graph.plot([0, 0], [-11, 11], pen=pen)  # Y-axis
 
-# Create sliders
-ax_inner = plt.axes([0.6, 0.7, 0.3, 0.05])
-ax_outer = plt.axes([0.6, 0.6, 0.3, 0.05])
-slider_inner = Slider(ax_inner, 'Inner Circle', 0.1, 10.0, valinit=1, valfmt=None, valstep=0.1)
-slider_outer = Slider(ax_outer, 'Outer Circle', 0.1, 10.0, valinit=2, valfmt=None, valstep=0.1)
+        # Connect parameter changes to circle update
+        self.params.param('Inner Radius').sigValueChanged.connect(self.update_circles)
+        self.params.param('Outer Radius').sigValueChanged.connect(self.update_circles)
+        self.params.param('Inner Radius').sigValueChanged.connect(self.radius_change)
+        self.params.param('Outer Radius').sigValueChanged.connect(self.radius_change)
+        self.params.param('Outer Radius').sigValueChanged.connect(self.steps_loop)
+        self.params.param('Inner Radius').sigValueChanged.connect(self.steps_loop)
+        self.params.param('Steps').sigValueChanged.connect(self.steps_loop)
+        
+        # Create a plot item for the starting point
+        self.starting_point = pg.PlotDataItem(pen=None, symbolBrush='g', symbolSize=10)
+        self.plot_graph.addItem(self.starting_point)
 
-# Create textboxes
-tb_inner = TextBox(plt.axes([0.6, 0.5, 0.3, 0.05]), 'Inner:', initial=str(inner_circle.radius))
-tb_outer = TextBox(plt.axes([0.6, 0.4, 0.3, 0.05]), 'Outer:', initial=str(outer_circle.radius))
+        # Create a plot item for the tangent line
+        self.tangent_line = pg.PlotDataItem(pen=pg.mkPen('g', width=2))
+        self.plot_graph.addItem(self.tangent_line)
 
-# Display intersection point below sliders
-intersection_point, = ax1.plot(0, 2, 'ro')  # 'ro' means red circle
-intersection_text = plt.text(0.6, 0.3, f'Intersection: {intersection_point}', transform=plt.gcf().transFigure)
+        # Draw circles
+        self.inner_circle = pg.PlotDataItem(pen=pg.mkPen('b', width=2))
+        self.outer_circle = pg.PlotDataItem(pen=pg.mkPen('r', width=2))
+        self.plot_graph.addItem(self.inner_circle)
+        self.plot_graph.addItem(self.outer_circle)
+        self.init_circles()
+        
+        # Add plot to layout
+        self.layout.addWidget(self.plot_graph)
+    
+    def init_circles(self):
+        inner_radius = self.params.param('Inner Radius').value()
+        outer_radius = self.params.param('Outer Radius').value()
 
-# Tangent line
-tangent_line, = ax1.plot([], [], color='green', linewidth=1)
+        # Calculate points for inner circle
+        theta = np.linspace(0, 2*np.pi, 100)
+        x = inner_radius * np.cos(theta)
+        y = inner_radius * np.sin(theta)
+        self.inner_circle.setData(x, y)
 
-# Update function for sliders and textboxes
-def update(val):
-    inner_radius = slider_inner.val
-    outer_radius = slider_outer.val
+        # Calculate points for outer circle
+        x = outer_radius * np.cos(theta)
+        y = outer_radius * np.sin(theta)
+        self.outer_circle.setData(x, y)
 
-    # Clamp max values of sliders
-    slider_inner.valmax = outer_radius
-    slider_outer.valmin = inner_radius
+        # Initialize starting point
+        self.steps_loop(self.params.param('Outer Radius'), outer_radius)
 
-    tb_inner.set_val(str(inner_radius))
-    tb_outer.set_val(str(outer_radius))
-    inner_circle.set_radius(inner_radius)
-    outer_circle.set_radius(outer_radius)
+    def update_circles(self, param, value):
+        # Calculate points for inner circle
+        theta = np.linspace(0, 2*np.pi, 100)
+        x = value * np.cos(theta)
+        y = value * np.sin(theta)
+        if param.name() == 'Inner Radius':
+            self.inner_circle.setData(x, y)
+        else:
+            self.outer_circle.setData(x, y)
+    
+    def radius_change(self, param, value):
+        if param.name() == 'Inner Radius':
+            self.params.param('Outer Radius').setLimits((value, 10))
+        else:  # param.name() == 'Outer Radius'
+            self.params.param('Inner Radius').setLimits((1, value))
 
-    # Update intersection point and its display
-    intersection_coords = (0, outer_radius)
-    intersection_text.set_text(f'Intersection: {intersection_coords}')
-    intersection_point.set_data(*intersection_coords)
+        for line in self.tangent_lines:
+            self.plot_graph.removeItem(line)
+            self.tangent_lines.clear()
 
-    # Calculate the slope of the line
-    if inner_radius < outer_radius:
-        x_intercept = np.sqrt(outer_radius ** 2 - inner_radius ** 2)
-        slope = x_intercept / -inner_radius
-        x_vals = np.linspace(-x_intercept, x_intercept, 100) + 0
-        y_vals = slope * (x_vals - 0) + outer_radius
-        tangent_line.set_data(x_vals, y_vals)
-    else:
-        tangent_line.set_data([], [])
+    def steps_loop(self, param, value):
+        outer_radius = self.params.param('Outer Radius').value()
+        outer_diameter = 2 * outer_radius
+        inner_radius = self.params.param('Inner Radius').value()
+        inner_diameter = 2 * inner_radius
+        steps = self.params.param('Steps').value()
+        
+        # Create a list to store the lines
+        self.tangent_lines = []
+        
+        # Calculate the length of the line
+        length = np.sqrt(outer_diameter**2 - (inner_diameter**2))
+        
+        # Calculate the starting angle (in radians)
+        angle = np.degrees(np.arcsin(inner_diameter / outer_diameter))
+
+        # Set the current point to the starting point (0, outer_radius)
+        current_point_x = 0
+        current_point_y = outer_radius
+
+        for _ in range(steps):
+            # Calculate the direction of the line from the current point to the origin
+            direction_x = -current_point_x
+            direction_y = -current_point_y
+
+            # Scale the direction vector to the desired length
+            scale = length / np.sqrt(direction_x**2 + direction_y**2)
+            direction_x *= scale
+            direction_y *= scale
+
+            # Rotate the direction vector by the desired angle
+            angle_rad = np.radians(angle)
+            rotated_x = direction_x * np.cos(angle_rad) - direction_y * np.sin(angle_rad)
+            rotated_y = direction_x * np.sin(angle_rad) + direction_y * np.cos(angle_rad)
+
+            # Calculate the coordinates of the tangent point
+            tangent_point_x = current_point_x + rotated_x
+            tangent_point_y = current_point_y + rotated_y
+
+            # Create a new line and add it to the list
+            tangent_line = pg.PlotDataItem([current_point_x, tangent_point_x], [current_point_y, tangent_point_y], pen=pg.mkPen('g', width=2))
+            self.tangent_lines.append(tangent_line)
+
+            # Add the line to the plot
+            self.plot_graph.addItem(tangent_line)
+
+            # Update the current point to the tangent point for the next iteration
+            current_point_x = tangent_point_x
+            current_point_y = tangent_point_y
 
 
-    fig.canvas.draw_idle()
-
-# Update function for textboxes
-def update_textbox(val):
-    inner_radius = min(float(tb_inner.text), 10.0)  # Ensure value is not greater than 10
-    outer_radius = min(float(tb_outer.text), 10.0)  # Ensure value is not greater than 10
-
-    # Ensure inner radius is not larger than outer radius
-    if inner_radius > outer_radius:
-        inner_radius = outer_radius
-        tb_inner.set_val(str(inner_radius))  # Update the textbox value
-
-    # Ensure outer radius is not smaller than inner radius
-    if outer_radius < inner_radius:
-        outer_radius = inner_radius
-        tb_outer.set_val(str(outer_radius))  # Update the textbox value
-
-    slider_inner.set_val(inner_radius)
-    slider_outer.set_val(outer_radius)
-    update(None)
-
-# Call update function when textbox value is changed
-tb_inner.on_submit(update_textbox)
-tb_outer.on_submit(update_textbox)
-
-# Call update function when slider value is changed
-slider_inner.on_changed(update)
-slider_outer.on_changed(update)
-
-# Show plot
-plt.show()
+app = QtWidgets.QApplication([])
+main = MainWindow()
+main.show()
+app.exec()
